9c0ff617e333bbffa84f7729ef08289d8c76fd89
James Hrisho <james.hrisho@gmail.com>
2020-07-23 14:57:54 -0400

    Support UMD

---
+++ packages/molasses-js/webpack.config.production.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const webpack = require("webpack")
+
+const config = {
+  module: {
+    rules: [
+      {
+        test: /(\.js|\.jsx)$/,
+        use: { loader: "babel-loader" },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts(x?)$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    library: "MolassesJS",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".tsx", ".ts"],
+  },
+}
+config.plugins = [
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  }),
+]
+config.mode = "production"
+module.exports = config
